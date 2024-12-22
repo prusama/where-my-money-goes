2a5497d1f4934dd2e214903ee2430758f623bcf7
prusama <marek.prusa.17@gmail.com>
2024-12-22 22:25:43 +0100

    Integrate TailwindCSS into PrimeNG

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -13,7 +13,13 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: Aura,
+        options: {
+          cssLayer: {
+            name: 'primeng',
+            order: 'tailwind-base, primeng, tailwind-utilities'
+          }
+        }
       }
     })
   ]
