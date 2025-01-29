ed13ea2faad39009594fbb394f4262a43e6def2c
prusama <marek.prusa.17@gmail.com>
2025-01-29 22:19:41 +0100

    Add AutoMapper

---
+++ server/Api/Models/DTO/TransactionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Models.DTO;
+
+public class TransactionDto
+{
+    public Guid Id { get; set; }
+    public DateTime Date { get; set; }
+    public string Type { get; set; }
+    public decimal Amount { get; set; }
+    public string Currency { get; set; }
+    public string AccountName { get; set; }
+}
