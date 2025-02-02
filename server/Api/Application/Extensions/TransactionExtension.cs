86056baa6ca6975d6f47c6e3dc79e9a192ec9c1b
prusama <marek.prusa.17@gmail.com>
2025-02-02 16:55:20 +0100

    Add transaction DTO

---
+++ server/Api/Application/Extensions/TransactionExtension.cs
@@ -0,0 +1,20 @@
+﻿using Api.Application.DTOs;
+using Api.Domain.Entities;
+
+namespace Api.Application.Extensions;
+
+public static class TransactionExtension
+{
+    public static TransactionDto ToDto(this Transaction transaction)
+    {
+        return new TransactionDto
+        {
+            Id = transaction.Id,
+            Date = transaction.Date,
+            Type = transaction.Type,
+            Amount = transaction.Amount,
+            Currency = transaction.Currency,
+            AccountName = transaction.AccountName,
+        };
+    }
+}
