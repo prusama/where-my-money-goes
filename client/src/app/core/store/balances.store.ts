c6e9c099d7068402796e6a5bae7dbe49f54a2a6b
prusama <marek.prusa.17@gmail.com>
2025-02-09 22:03:50 +0100

    Remove ngrx entities

--- client/src/app/core/store/balances.store.ts
+++ client/src/app/core/store/balances.store.ts
@@ -1,6 +1,4 @@
-import {YearBalance} from '../model/year-balance.model';
-import {deepComputed, DeepSignal, patchState, signalStore, withComputed, withMethods, withState} from '@ngrx/signals';
-import {addEntities, addEntity, withEntities} from '@ngrx/signals/entities';
+import {deepComputed, DeepSignal, signalStore, withComputed, withMethods, withState} from '@ngrx/signals';
 import {computed, signal} from '@angular/core';
 import {YEAR_BALANCE_MOCK} from '../mocks/year-balance.mock';
 import {MonthBalance} from '../model/month-balance.model';
@@ -19,26 +17,8 @@ const initialState: BalancesState = {
 export const BalancesStore = signalStore(
   { providedIn: 'root' },
   withState(initialState),
-  withEntities<YearBalance>(),
   withComputed(() => ({
     yearBalances: computed(() => YEAR_BALANCE_MOCK)
-  })),
-  withMethods((store) => ({
-    addYear(year: number): void {
-      const newBalance: YearBalance = {
-        id: Math.random().toString(),
-        year: year,
-        months: [],
-        yearIncomes: 15000,
-        yearExpenses: 10000,
-        yearBalance: 5000,
-      };
-      patchState(store, addEntity(newBalance))
-    },
-    loadYears(): void {
-      patchState(store, addEntities(YEAR_BALANCE_MOCK))
-      patchState(store, { loading: false })
-    }
   })),
   withMethods((store) => ({
     getBalancesGroupedByMonths(year: number): Array<DeepSignal<MonthBalance>> {
