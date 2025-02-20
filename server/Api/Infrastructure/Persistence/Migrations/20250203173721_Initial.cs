adc26c2d0efa2992492c57ff943d03ba4d5289c9
prusama <marek.prusa.17@gmail.com>
2025-02-20 22:04:13 +0100

    Add migrations

---
+++ server/Api/Infrastructure/Persistence/Migrations/20250203173721_Initial.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Api.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AccountName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Transactions",
+                columns: new[] { "Id", "AccountName", "Amount", "Currency", "Date", "Type" },
+                values: new object[,]
+                {
+                    { new Guid("e60c279b-3048-4106-8d9f-817c4c213ae9"), "Some Company", 41000m, "CZK", new DateTime(2021, 12, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 },
+                    { new Guid("ea9c1742-75ee-4482-b1a9-64c1cb3795f1"), "Tesco", 1000m, "CZK", new DateTime(2021, 12, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
---
+++ server/Api/Infrastructure/Persistence/Migrations/20250220195037_Increase_Expense_Seed.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Increase_Expense_Seed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: new Guid("ea9c1742-75ee-4482-b1a9-64c1cb3795f1"),
+                column: "Amount",
+                value: 20000m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: new Guid("ea9c1742-75ee-4482-b1a9-64c1cb3795f1"),
+                column: "Amount",
+                value: 1000m);
+        }
+    }
+}
